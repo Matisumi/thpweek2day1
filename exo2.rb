c9e7eb010ae64dbf6a97d8534ced307f6c1dad92
Sabine <matisuo@gmail.com>
2018-01-15 14:01:25 +0100

    adding exex 02

---
+++ exo2.rb
@@ -0,0 +1,40 @@
+def chiffre_de_cesar (str, mod)
+	cryptd = ''
+	puts str
+	str.each_char do |char|
+		asci = char.ord
+		puts asci
+		if asci < 91 && asci > 64
+			asci = asci + mod
+			while asci > 90
+				asci = asci - 26
+			end
+			while asci < 65 
+				asci = asci + 26
+			end
+		
+		elsif (asci < 123 && asci > 96)
+			asci = asci + mod
+			while asci > 122
+				asci = asci - 26
+			end
+			while asci < 97
+				asci = asci + 26
+			end
+		end
+		cryptd = cryptd + asci.chr
+	end
+	
+	return cryptd;
+end
+		
+	
+puts chiffre_de_cesar("What a string!", 57)	
+
+
+
+=begin
+	
+#	str.each do |char|
+	
+=end
